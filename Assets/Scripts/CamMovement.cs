512428e959c1c514c78ae47308ed67e7dc6e1f73
Seb18456 <sebastian18678@gmail.com>
2024-10-05 13:50:40 -0400

    Added Camera Movement
    Rough camera movement.

--- Assets/Scripts/CamMovement.cs
+++ Assets/Scripts/CamMovement.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CamMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Rotation Properties")]
+    public float mouseX;
+    public float mouseY;
+    public float mouseSpeed = 1f;
+    public Vector2 clampAngles;
+    private float xRotation, yRotation = 0f;
+    private Camera cam;
+
+    [Header("Movement Properties")]
+    public float vertical;
+    public float horizontal;
+    public float moveSpeed = 1f;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        mouseX = Input.GetAxis("Mouse X") * (mouseSpeed * 10f * Time.deltaTime);
+        mouseY = Input.GetAxis("Mouse Y") * (mouseSpeed * 10f * Time.deltaTime);
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
         
+        yRotation -= mouseY;
+        xRotation += mouseX;
+
+        transform.rotation = Quaternion.Euler(0f, xRotation, 0f);
+        cam.transform.rotation = Quaternion.Euler(Mathf.Clamp(yRotation, -clampAngles.x, clampAngles.x), Mathf.Clamp(xRotation, -clampAngles.y, clampAngles.y), 0f);
+
+        moveDirection = transform.forward * vertical + transform.right * horizontal;
+        rb.AddForce(moveDirection.normalized * 20f * moveSpeed, ForceMode.Force);
     }
 }
