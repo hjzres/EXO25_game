d591f83727fda11088935b868af965f216a2e07d
Seb18456 <sebastian18678@gmail.com>
2024-10-06 18:04:39 -0400

    Rempved Cam Movement

--- Assets/Scripts/CamMovement.cs
+++ Assets/Scripts/CamMovement.cs
@@ -13,34 +13,19 @@ public class CamMovement : MonoBehaviour
     private float xRotation, yRotation = 0f;
     private Camera cam;
 
-    [Header("Movement Properties")]
-    public float vertical;
-    public float horizontal;
-    public float moveSpeed = 1f;
-    private Rigidbody rb;
-    private Vector3 moveDirection;
-
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         cam = GetComponentInChildren<Camera>();
-        rb.freezeRotation = true;
     }
 
     private void Update()
     {
         mouseX = Input.GetAxis("Mouse X") * (mouseSpeed * 10f * Time.deltaTime);
         mouseY = Input.GetAxis("Mouse Y") * (mouseSpeed * 10f * Time.deltaTime);
-        vertical = Input.GetAxisRaw("Vertical");
-        horizontal = Input.GetAxisRaw("Horizontal");
-        
         yRotation -= mouseY;
         xRotation += mouseX;
 
         transform.rotation = Quaternion.Euler(0f, xRotation, 0f);
         cam.transform.rotation = Quaternion.Euler(Mathf.Clamp(yRotation, -clampAngles.x, clampAngles.x), Mathf.Clamp(xRotation, -clampAngles.y, clampAngles.y), 0f);
-
-        moveDirection = transform.forward * vertical + transform.right * horizontal;
-        rb.AddForce(moveDirection.normalized * 20f * moveSpeed, ForceMode.Force);
     }
 }
