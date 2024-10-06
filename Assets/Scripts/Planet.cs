f963844dd52aa438f720c1fff646eb12ea7d859f
Andrei <142443853+hjzres@users.noreply.github.com>
2024-10-05 20:31:56 -0400

    planet class

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,14 @@
+public class Planet
+{
+	int id;
+	string name;
+	int size;
+	int distance;
+	string material;
+	bool gas;
+	int ring; // If done do this otherwise no
+	string colour;
+	float temp;
+	float starTemp;
+	int starSize;
+}
