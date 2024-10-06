998b92cd73d9f69ebd1020e736a260893333d2cf
Seb18456 <sebastian18678@gmail.com>
2024-10-05 21:01:53 -0400

    Added Rock Normal Texture

--- Assets/Scripts/ExoplanetTerrain.cs
+++ Assets/Scripts/ExoplanetTerrain.cs
@@ -12,6 +12,8 @@ public class ExoplanetTerrain : MonoBehaviour
     public int height = 256;
     public float scale = 1f;
     public int octaves = 1;
+    public float lacunarity = 1.0f;
+    public float persistence = 1.0f;
     public float[,] noiseMap;
 
     [Header("Texture Properties")]
@@ -22,7 +24,7 @@ public class ExoplanetTerrain : MonoBehaviour
     {
         Terrain terrain = Terrain.activeTerrain;
         TerrainData data = terrain.terrainData;
-        noiseMap = PerlinNoise2D(width, height, scale, octaves);
+        noiseMap = PerlinNoise2D(width, height, scale, octaves, lacunarity, persistence);
 
         data.heightmapResolution = width + 1;
         data.size = new Vector3(width, 100, height);
@@ -53,7 +55,7 @@ public class ExoplanetTerrain : MonoBehaviour
         }
     }
 
-    public float[,] PerlinNoise2D(float width, float length, float scale = 1f, int octaves = 1)
+    public float[,] PerlinNoise2D(float width, float length, float scale = 1f, int octaves = 1, float lacunarity = 1f, float persistence = 1f)
     {
         float[,] noiseMap = new float[(int)width, (int)length];
 
@@ -73,19 +75,42 @@ public class ExoplanetTerrain : MonoBehaviour
         {
             for (int y = 0; y < length; y++)
             {
+                //float multiplier = 0.2f;
+                float frequency = 1f;
+                float amplitude = 1f;
                 float height = 0;
 
                 for (int i = 0; i < octaves; i++)
                 {
-                    float sampleX = (float)x / width * scale + octaveOffset[i].x;
-                    float sampleY = (float)y / length * scale + octaveOffset[i].y;
+                    float sampleX = (float)x / width * scale * frequency + octaveOffset[i].x;
+                    float sampleY = (float)y / length * scale * frequency + octaveOffset[i].y;
                     float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                    height += perlinValue;
+                    height += perlinValue * amplitude;
+                    frequency *= lacunarity;
+                    amplitude *= persistence;
                 }
 
-                float multiplier = height < 0.3f ? 0.5f : (height >= 0.3f || height <= 0.9f ? 1f : 2.3f);
-                height *= multiplier;
+                //float multiplier = height < 0.3f ? 0.2f : (height >= 0.3f || height <= 0.9f ? 1f : 2.3f);
+                //height *= multiplier;
                 noiseMap[x, y] = height;
+            }
+        }
+
+        int x1 = 0;
+        float multiplier = 0;
+        
+        while (x1 < width / 2)
+        {
+            multiplier = x1 * x1;
+            noiseMap[x1, 0] *= multiplier;
+            x1++;
+        }
+
+        for (int i = 0; i < noiseMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < noiseMap.GetLength(1); j++)
+            {
+
             }
         }
 
